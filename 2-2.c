8456f36e34430b4e12d09a6ba20f3bfc887f9fee
kaizawawa <kai109870@gmail.com>
2019-11-25 15:13:11 +0900

    git change 2-2.c

--- 2-2.c
+++ 2-2.c
@@ -5,8 +5,19 @@ int main()
 	int i, lim;
 	char c, s[MAX];
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
+	for (i = 0; i < lim - 1; ++i)
 	{
-		s[i] = c;
+		if ((c = getchar()) == '\n')
+		{
+			i = lim;
+		}
+		else if (c == EOF)
+		{
+			i = lim;
+		}
+		else
+		{
+			s[i] = c;
+		}
 	}
 }
